18d427afeb1e8d0894ff54bae1ac574d178b5a70
rphaley <rhaley@depaul.edu>
2022-01-11 22:19:18 -0600

    update ClassIDs

--- clone.py
+++ clone.py
@@ -91,17 +91,17 @@ async def clonePod(connection,masterPodName,datastore,newPodName,podStart,podSto
         print('='*20 + 'Setting ACL on new pod' + '='*20 )
         await connection.pod_update(pod_id=CurrentPodId,pod_acl_enabled=True)
         if '378' in pc_clone_specs[0]['clone_name']:
-            cls_id = 1
+            cls_id = 11
         elif '380' in pc_clone_specs[0]['clone_name']:
-            cls_id = 2
+            cls_id = 12
         elif '388' in pc_clone_specs[0]['clone_name']:
             cls_id = 3
         elif '395' in pc_clone_specs[0]['clone_name']:
-            cls_id = 4
+            cls_id = 13
         elif '340' in pc_clone_specs[0]['clone_name']:
-            cls_id = 5
+            cls_id = 18
         elif '594' in pc_clone_specs[0]['clone_name']:
-            cls_id = 6
+            cls_id = 15
         try:
             acls = await connection.pod_acl_add(com_id=1, pod_id=CurrentPodId, acc_id=None, cls_id=cls_id, team=None)
         except:
